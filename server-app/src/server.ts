8aa63c59e1c4e02d841e0ac2183101df64e5f57c
Samuel Macedo <smauel.macfer@gmail.com>
2024-06-02 20:58:06 -0300

    fix: edit server.ts

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -11,11 +11,10 @@ const port = process.env.PORT || 5001
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
-
+		origin: ['https://gift-chosen.vercel.app'],
+		methods: ['GET', 'POST'],
 		credentials: true,
 	}),
 )
