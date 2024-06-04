36ee11ce0c8030f7b85fd255aea403e99c43131f
Samuel Macedo <smauel.macfer@gmail.com>
2024-06-04 16:00:52 -0300

    fix: edit Header

--- client-app/src/components/Header/Header.tsx
+++ client-app/src/components/Header/Header.tsx
@@ -1,14 +1,9 @@
 import { Container, CoupleNames } from './styled'
 
-import flowers from '../../assets/flowers.png'
-
 export const Header = () => {
 	return (
 		<Container>
-			<CoupleNames>
-				<h1>Samuel & Vitória</h1>
-				<img src={flowers} />
-			</CoupleNames>
+			<CoupleNames></CoupleNames>
 		</Container>
 	)
 }
