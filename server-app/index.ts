2df75781db6e901e5e87d39195aa8c9efb266a5c
Samuel Macedo <smauel.macfer@gmail.com>
2024-06-04 15:10:15 -0300

    fix: edit server.ts

--- server-app/index.ts
+++ server-app/index.ts
@@ -60,54 +60,19 @@
 
 // app.listen(port, () => console.log(`listening on ${port}`))
 
-import express from 'express'
+import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDb from './src/config/dbConfig'
+import giftRoutes from './src/routes/giftRoutes'
+import express from 'express'
 
-interface IUser {
-	id: string
-	name: string
-	email: string
-}
+dotenv.config()
+connectDb()
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT || 3001
-
-const users: IUser[] = []
-
-app.get('/', (req, res) => {
-	return res.json('hello world')
-})
-
-app.get('/users', (req, res) => {
-	return res.json(users)
-})
-
-app.post('/users', (req, res) => {
-	const { name, email } = req.body
-
-	const newUser = {
-		id: Math.random().toString(36),
-		name,
-		email,
-	}
-
-	users.push(newUser)
-	return res.json(newUser)
-})
-
-app.delete('/users/:id', (req, res) => {
-	const { id } = req.params
-
-	const index = users.findIndex((user) => user.id === id)
-
-	if (index < 0) {
-		return res.status(404).json({ Error })
-	}
-
-	users.splice(index, 1)
-	return res.status(204).json()
-})
+app.use('/', giftRoutes)
 
 app.listen(port, () => console.log(`listening on ${port}`))
