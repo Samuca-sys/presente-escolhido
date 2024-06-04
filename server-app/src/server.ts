f57236eefc13e94d05d272ae4f90833010e141ee
Samuel Macedo <smauel.macfer@gmail.com>
2024-06-04 10:48:54 -0300

    fix: edit server.ts

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -1,25 +1,40 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import connectDb from './config/dbConfig'
-import giftRoutes from './routes/giftRoutes'
-dotenv.config()
-connectDb()
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import cors from 'cors'
+// import connectDb from './config/dbConfig'
+// import giftRoutes from './routes/giftRoutes'
+// dotenv.config()
+// connectDb()
 
-const port = process.env.PORT || 3001
+// const port = process.env.PORT || 3001 //back e tem q ser igual a VITE_SERVER_URL
+
+// const app = express()
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(
+// 	cors({
+// 		origin: 'http://localhost:3000', //front
+// 		credentials: true,
+// 	}),
+// )
+
+// app.use('/api/', giftRoutes)
+
+// app.listen(port, () => {
+// 	console.log(`Server started successfully on port : ${port}`)
+// })
+
+import express from 'express'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(
-	cors({
-		origin: 'http://localhost:3000',
-		credentials: true,
-	}),
-)
+app.use(cors())
 
-app.use('/api/', giftRoutes)
+const port = process.env.PORT || 3001
 
-app.listen(port, () => {
-	console.log(`Server started successfully on port : ${port}`)
+app.get('/', (req, res) => {
+	return res.json('hello world')
 })
+
+app.listen(port, () => console.log(`listening on ${port}`))
